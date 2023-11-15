cd0abe1823ad00a87bd1d616b06f7fb9dc06bb71
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-11-15 15:23:08 +0100

    Version 0.5 (#26)
    Added dust effect to player
    Added Coyote jump
    Added blaster for player and static bullet

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This project is based on the [1-bit-godot](https://courses.heartgamedev.com/p/1-bit-godot-4-course) by [Heartbeast](https://www.youtube.com/c/uheartbeast).
+This project is based on the [1-Bit Godot 4 Course](https://courses.heartgamedev.com/p/1-bit-godot-4-course) from [Heartbeast](https://www.youtube.com/c/uheartbeast).
 
 ![Screenshot of platform game](startscreen.png)
--- player/PlayerChar.gd
+++ player/PlayerChar.gd
@@ -1,6 +1,8 @@
 class_name PlayerChar
 extends CharacterBody2D
 
+const DustEffectScene = preload("res://effects/dust_effect.tscn")
+
 @export var acceleration = 512
 @export var max_velocity = 64
 @export var friction = 256
@@ -10,11 +12,13 @@ extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var player_blaster = $PlayerBlaster
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	
-	var input_axis = Input.get_axis("ui_left", "ui_right")
+	var input_axis = Input.get_axis("move_left", "move_right")
 	
 	if is_moving(input_axis):
 		apply_acceleration(delta, input_axis)
@@ -22,8 +26,23 @@ func _physics_process(delta: float) -> void:
 		apply_friction(delta)
 	
 	jump_check()
+	
+	if(Input.is_action_just_pressed("fire")):
+		player_blaster.fire_bullet()
+	
 	update_animation(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_edge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_edge:
+		coyote_jump_timer.start()
+
+
+func create_dust_effect() -> void:
+	var dust_effect = DustEffectScene.instantiate()
+	var main = get_tree().current_scene
+	main.add_child(dust_effect)
+	dust_effect.global_position = global_position
 
 
 func is_moving(input_axis: float) -> bool:
@@ -45,18 +64,23 @@ func apply_friction(delta: float) -> void:
 
 
 func jump_check() -> void:
-	if  is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+	if  is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_force
-	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < -jump_force / 2.0:
+	if not is_on_floor():
+		if Input.is_action_just_released("jump") and velocity.y < -jump_force / 2.0:
 			velocity.y = -jump_force / 2.0
 
 
 func update_animation(input_axis: float) -> void:
+	sprite_2d.scale.x = sign(get_local_mouse_position().x)
+	
+	if abs(sprite_2d.scale.x) != 1: 
+		sprite_2d.scale.x = 1
+	
 	if is_moving(input_axis):
 		animation_player.play("run")
-		sprite_2d.scale.x = sign(input_axis)
+		animation_player.speed_scale = input_axis * sprite_2d.scale.x
 	else:
 		animation_player.play("idle")
 	
