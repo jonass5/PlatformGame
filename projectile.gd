498b280da35923b553a922b6bc83d613d38e7753
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-11-17 05:39:28 +0100

    Create projectile

---
+++ projectile.gd
@@ -0,0 +1,19 @@
+class_name Projectile
+extends Node2D
+
+@export var speed = 200
+
+var velocity = Vector2.ZERO
+
+func _ready():
+	velocity.x = speed
+	velocity = velocity.rotated(rotation)
+
+
+func _process(delta: float):
+	position += velocity * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
