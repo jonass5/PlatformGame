54660ee84aaabcfaaa4f7612eef6d33035e49d32
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-11-23 06:03:18 +0100

    Added walking enemie

---
+++ enemies/walking_enemy.gd
@@ -0,0 +1,35 @@
+class_name WalkingEnemy
+extends CharacterBody2D
+
+@export var speed: float = 30.0
+@export var turns_on_ledges: bool = true
+
+var gravity: float = 200.0
+var direction: float = 1.0
+
+@onready var sprite_2d = $Sprite2D
+@onready var floor_cast = $FloorCast
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if is_on_wall():
+		turn_around()
+
+	if is_on_ledge() and turns_on_ledges:
+		turn_around()
+
+	velocity.x = direction * speed
+	sprite_2d.scale.x = direction
+
+	move_and_slide()
+
+
+func is_on_ledge() -> bool:
+	return is_on_floor() and not floor_cast.is_colliding()
+
+
+func turn_around() -> void:
+	direction *= -1.0
