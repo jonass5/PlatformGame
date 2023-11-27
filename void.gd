d4393ed96874feec5f5271589d687a038699b9f2
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2023-11-27 16:08:54 +0100

    Replace signal with simple method call

--- void.gd
+++ void.gd
@@ -1,9 +1,7 @@
 class_name Void
 extends Area2D
 
-signal reached_void
-
 
 func _on_body_exited(body):
-	self.reached_void.connect(body.reached_void)
-	reached_void.emit()
+	if body is PlayerChar:
+		body.reached_void()
