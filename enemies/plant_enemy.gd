e404b344cd3ed4480bbac83d6739acb1143800b4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-08 05:34:24 +0100

    Projectile Polish

--- enemies/plant_enemy.gd
+++ enemies/plant_enemy.gd
@@ -4,7 +4,7 @@ extends Node2D
 const EnemyBulletScene = preload("res://enemies/enemy_bullet.tscn")
 
 @export var bullet_speed = 30
-@export var spread = 30
+@export var spread = 45
 
 @onready var stats = $Stats
 @onready var bullet_spawn_point = $BulletSpawnPoint
@@ -14,10 +14,9 @@ const EnemyBulletScene = preload("res://enemies/enemy_bullet.tscn")
 func fire_bullet():
 	var bullet = Utils.instanciate_scene_on_world(EnemyBulletScene, bullet_spawn_point.global_position) as Projectile
 	var direction = global_position.direction_to(fire_direction.global_position)
-	bullet.rotation = direction.angle()
-	bullet.rotate(randf_range(-deg_to_rad(spread/2), deg_to_rad(spread/2)))
-	bullet.speed = bullet_speed
-	bullet.update_velocity()
+	var velocity = direction.normalized() * bullet_speed
+	velocity = velocity.rotated(randf_range(-deg_to_rad(spread/2), deg_to_rad(spread/2)))
+	bullet.velocity = velocity
 
 
 func _on_stats_no_health():
