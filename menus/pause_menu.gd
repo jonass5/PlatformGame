e9bae77f561ea67dbe2920eebcec3ed4ede11ced
Sittauer, J. (Jonas) <Jonas.Sittauer@ing.de>
2023-12-20 11:00:07 +0100

    Added pause menu

---
+++ menus/pause_menu.gd
@@ -0,0 +1,20 @@
+extends ColorRect
+
+var paused = false :
+	set(value):
+		paused = value
+		get_tree().paused = paused
+		visible = paused
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		paused = !paused
+
+
+func _on_resume_button_pressed():
+	paused = false
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
