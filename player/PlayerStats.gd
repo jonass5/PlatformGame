6b60affc9fcfdf6d013f1cbde9acf6bc7faf7776
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-12-22 06:01:09 +0100

    Added Missle UI

--- player/PlayerStats.gd
+++ player/PlayerStats.gd
@@ -1,5 +1,7 @@
 extends Stats
 
+signal missles_changed
+
 @export var max_missiles: int = 3 : set = set_max_missiles
 
 @onready var missiles: int = 3 : set = set_missiles
@@ -10,3 +12,4 @@ func set_max_missiles(value: int):
 
 func set_missiles(value: int):
 	missiles = clampi(value, 0, max_missiles)
+	missles_changed.emit()
