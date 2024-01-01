078a9e62f326e499901d9a3d1a564e4c2ca33601
Sittauer, J. (Jonas) <Jonas.Sittauer@ing.de>
2024-01-01 18:35:35 +0100

    Added storage function for powerups

--- player/powerup.gd
+++ player/powerup.gd
@@ -2,7 +2,16 @@ class_name Powerup
 extends Area2D
 
 
+func _ready():
+	await get_tree().process_frame
+	var id = WorldStash.get_id(self)
+	var freed = WorldStash.retrive(id, "freed")
+	if freed: queue_free()
+
+
 func pickup():
+	var id = WorldStash.get_id(self)
+	WorldStash.stash(id, "freed", true)
 	queue_free()
 
 
