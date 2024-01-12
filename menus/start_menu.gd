0652a351371476cd8d1e5d75b8663b3706dc27d9
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-01-12 09:36:55 +0100

    Hide quit button on Web

--- menus/start_menu.gd
+++ menus/start_menu.gd
@@ -1,5 +1,13 @@
-extends ColorRect
 class_name StartMenu
+extends ColorRect
+
+@onready var quit_game = $CenterContainer/VBoxContainer/QuitGame
+
+
+func _ready():
+	if OS.get_name() == "Web":
+		quit_game.hide()
+
 
 func _on_start_gane_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
