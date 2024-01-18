6fd8cd569e695df2d6b058056c3e6f21e2a63bdc
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-01-18 19:01:39 +0100

    Added keyboard and controller support to start_menu

--- menus/start_menu.gd
+++ menus/start_menu.gd
@@ -1,15 +1,17 @@
 class_name StartMenu
 extends ColorRect
 
+@onready var start_game = $CenterContainer/VBoxContainer/StartGame
 @onready var quit_game = $CenterContainer/VBoxContainer/QuitGame
 
 
 func _ready():
+	start_game.grab_focus()
 	if OS.get_name() == "Web":
 		quit_game.hide()
 
 
-func _on_start_gane_pressed():
+func _on_start_game_pressed():
 	Sound.play(Sound.click, 1.0, -10.0)
 	get_tree().change_scene_to_file("res://world.tscn")
 
