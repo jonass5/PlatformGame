1b862a4e53755bdf688eb9049cf1be5d450867db
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-01-21 08:26:17 +0100

    Support controller in game over menu

--- menus/game_over_menu.gd
+++ menus/game_over_menu.gd
@@ -1,6 +1,12 @@
 class_name GameOverMenu
 extends ColorRect
 
+@onready var load_button = $CenterContainer/VBoxContainer/LoadButton
+
+
+func _ready():
+	load_button.grab_focus()
+
 
 func _on_load_button_pressed():
 	Sound.play(Sound.click, 1.0, -10.0)
