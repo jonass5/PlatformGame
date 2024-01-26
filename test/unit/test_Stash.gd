65af1b847ece55f1262dfe8496e24a5436aaaae4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-01-26 06:22:24 +0100

    Added tests for Stash.retrieve

--- test/unit/test_Stash.gd
+++ test/unit/test_Stash.gd
@@ -10,6 +10,28 @@ func before_each():
 func after_each():
 	stash.free()
 
+
+func test_should_return_null_when_id_does_not_exist():
+	# act
+	var result = stash.retrieve("player", "missiles")
+
+	# assert
+	assert_null(result)
+
+
+func test_should_return_null_when_key_does_not_exist():
+	# arrange
+	stash.data = {
+		"player": { }
+	}
+
+	# act
+	var result = stash.retrieve("player", "missiles")
+
+	# assert
+	assert_null(result)
+
+
 func test_should_retrieve_value_when_value_exists():
 	# arrange
 	stash.data = {
@@ -17,9 +39,9 @@ func test_should_retrieve_value_when_value_exists():
 			"missiles": 3
 		}
 	}
-	
+
 	# act
 	var result = stash.retrieve("player", "missiles")
-	
+
 	# assert
 	assert_eq(result, 3)
