cd76b83fd207e32d883c58dbf5ed4446808d2099
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-02-02 06:02:54 +0100

    Moved stats files to enemies directory

--- enemies/stats.gd
+++ enemies/stats.gd
@@ -1,1 +1,24 @@
-extends Node2D
+class_name Stats
+extends Node
+
+
+@export var max_health = 3 : set = set_max_health
+@onready var health = max_health : set = set_health
+
+signal no_health
+signal health_changed
+signal max_health_changed
+
+
+func set_max_health(value: int) -> void:
+	max_health = value
+	max_health_changed.emit()
+
+
+func set_health(value: int) -> void:
+	health = clamp(value, 0, max_health)
+
+	health_changed.emit()
+
+	if(health <= 0):
+		no_health.emit()
