eda76e9c136fdb32367393ac19cd29439a1747dd
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-02-19 15:53:17 +0100

    Added test to add more missiles

--- test/unit/test_MissilePowerup.gd
+++ test/unit/test_MissilePowerup.gd
@@ -1,6 +1,6 @@
 extends GutTest
 
-var missile_powerup = null
+var missile_powerup: MissilePowerup = null
 
 
 func before_each():
@@ -9,6 +9,8 @@ func before_each():
 
 func after_each():
 	missile_powerup.free()
+	PlayerStats.missiles = 0
+	PlayerStats.max_missiles = 0
 
 
 func test_should_increase_player_missles_max_by_three_when_picked_up():
@@ -20,5 +22,20 @@ func test_should_increase_player_missles_max_by_three_when_picked_up():
 	missile_powerup.pickup()
 
 	# assert
-	assert_eq(PlayerStats.max_missiles, 3)
 	assert_eq(PlayerStats.missiles, 3)
+	assert_eq(PlayerStats.max_missiles, 3)
+	
+	
+func test_should_increase_player_missles_max_by_three_when_already_missiles_are_carried():
+	# arrange
+	PlayerStats.max_missiles = 6
+	PlayerStats.missiles = 3
+
+	# act
+	missile_powerup.pickup()
+
+	# assert
+	assert_eq(PlayerStats.missiles, 6)
+	assert_eq(PlayerStats.max_missiles, 9)
+	
+	
