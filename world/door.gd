03a0cdecc7d4bc7271dc7af39b92a6af2c9a201e
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-03-27 16:29:10 +0100

    Extendig door to also enter and leave it vertically

--- world/door.gd
+++ world/door.gd
@@ -3,11 +3,16 @@ extends Area2D
 
 @export_file("*.tscn") var new_level_path
 @export var connection: DoorConnection
+@export var door_type: DoorType = DoorType.VERTICAL
 
 var active = false
 
+enum DoorType { VERTICAL, HORIZONTAL }
+
 @onready var right_cast = $RightCast
 @onready var left_cast = $LeftCast
+@onready var down_cast = $DownCast
+@onready var up_cast = $UpCast
 
 
 func _physics_process(_delta: float):
@@ -17,17 +22,27 @@ func _physics_process(_delta: float):
 	if not active:
 		return
 	if overlaps_body(player) and new_level_path:
-		var player_direction = sign(player.velocity.x)
+		var player_direction
+		if door_type == DoorType.VERTICAL:
+			player_direction = sign(player.velocity.x)
+		else:
+			player_direction = sign(player.velocity.y)
 		var door_direction = get_direction()
 		if (player_direction == door_direction):
 			Events.door_entered.emit(self)
 
 
 func get_direction():
-	if left_cast.is_colliding():
-		return -1
-	if right_cast.is_colliding():
-		return 1
+	if door_type == DoorType.VERTICAL:
+		if left_cast.is_colliding():
+			return -1
+		if right_cast.is_colliding():
+			return 1
+	else:
+		if up_cast.is_colliding():
+			return -1
+		if down_cast.is_colliding():
+			return 1
 	return 0
 
 
