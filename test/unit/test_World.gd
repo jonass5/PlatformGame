0deff39d804b701b0c7447b6f2b35234ba3c0126
Torsten Wiederkehr <torsten.wiederkehr@ing.com>
2024-03-28 13:51:26 +0100

    Added test to find proper door in level with more than one door from the same level

--- test/unit/test_World.gd
+++ test/unit/test_World.gd
@@ -11,7 +11,7 @@ func after_each():
 	world.free()
 
 
-func test_should():
+func test_should_find_door_when_door_points_to_old_level():
 	# arrange
 	var src_door = Door.new()
 	var dest_door = Door.new()
@@ -28,4 +28,32 @@ func test_should():
 	# tear down
 	src_door.free()
 	dest_door.free()
+
+
+func test_should_find_door_when_door_points_to_old_level_and_door_connection_fits():
+	# arrange
+	var door_connection = DoorConnection.new()
+	var src_door = Door.new()
+	src_door.connection = door_connection
 	
+	var dest_door = Door.new()
+	dest_door.new_level_path = "level1"
+	dest_door.connection = door_connection
+	
+	var wrong_door = Door.new()
+	wrong_door.new_level_path = "level1"
+	wrong_door.connection = DoorConnection.new()
+	
+	var doors: Array[Node] = [ src_door, wrong_door, dest_door ]
+	var old_level_path = "level1"
+	
+	# act
+	var found_door = world.find_door(doors, src_door, old_level_path)
+	
+	# assert
+	assert_eq(found_door, dest_door)
+	
+	# tear down
+	src_door.free()
+	dest_door.free()
+
