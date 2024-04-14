e78bd61a2e117b879414989d3f3182f83fb5be14
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-04-14 14:48:33 +0200

    Removed out commented code

--- world/teleport.gd
+++ world/teleport.gd
@@ -10,11 +10,6 @@ const OFFSET = Vector2(0, -8)
 func _on_body_entered(body):
 	if body is Player:
 		teleport.call_deferred(body)
-		#MainInstances.world.load_level.call_deferred(new_level_path)
-		#var targets = get_tree().get_nodes_in_group("teleport")
-		#if not targets.is_empty():
-			#body.global_position = targets[0].global_position
-			#Utils.instanciate_scene_on_level(TeleportEffectScene, body.global_position + OFFSET)
 
 
 func teleport(body):
