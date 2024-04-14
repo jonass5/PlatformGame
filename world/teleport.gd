1941c7a3c35a34dfe5c3b900a9e5c0beebfc17f6
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-04-14 14:20:52 +0200

    Added teleport effect

--- world/teleport.gd
+++ world/teleport.gd
@@ -1,11 +1,15 @@
 class_name Teleport
 extends Area2D
 
+const TeleportEffectScene = preload("res://effects/teleport_effect.tscn")
+
 @export_file("*.tscn") var new_level_path
+
 
 func _on_body_entered(body):
 	if body is Player:
 		MainInstances.world.load_level(new_level_path)
 		var targets = get_tree().get_nodes_in_group("teleport")
 		body.global_position = targets[0].global_position
+		Utils.instanciate_scene_on_level(TeleportEffectScene, body.global_position + Vector2(0, -8))
 
