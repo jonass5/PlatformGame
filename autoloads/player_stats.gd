9e8817ebea30b242d512ef6c4d2f6e0b03e64c4e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-04-16 18:44:08 +0200

    Added PlayerStats.is_missiles_activated() to avoid direct access to properies of PlayerStats

--- autoloads/player_stats.gd
+++ autoloads/player_stats.gd
@@ -20,33 +20,37 @@ func set_missiles(value: int):
 	missiles_changed.emit()
 
 
-func reset():
+func reset() -> void:
 	max_health = starting_max_health
 	max_missiles = starting_max_missiles
 	refill()
 
 
-func refill():
+func refill() -> void:
 	health = max_health
 
 
-func stash_stats():
+func stash_stats() -> void:
 	WorldStash.stash("player", "max_health", max_health)
 	WorldStash.stash("player", "max_missiles", max_missiles)
 	WorldStash.stash("player", "missiles", missiles)
 
 
-func retrieve_stats():
+func retrieve_stats() -> void:
 	max_health = WorldStash.retrieve("player", "max_health")
 	max_missiles = WorldStash.retrieve("player", "max_missiles")
 	missiles = WorldStash.retrieve("player", "missiles")
 	refill()
 
 
-func has_missiles():
+func has_missiles() -> bool:
 	return missiles > 0
 
 
-func fire_missile():
+func fire_missile() -> int:
 	missiles -= 1
 	return missiles
+
+
+func is_missiles_activated() -> bool:
+	return max_missiles > 0
