0e9ae8007d6ac0a4d28885a3728095d1b9ee2aa0
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-08-25 06:29:31 +0200

    Reactivated intro

--- menus/start_menu.gd
+++ menus/start_menu.gd
@@ -18,8 +18,7 @@ func _ready():
 
 
 func _on_start_button_pressed():
-	#get_tree().change_scene_to_file("res://dialogic/opening_scene.tscn")
-	get_tree().change_scene_to_file("res://world/world.tscn")
+	get_tree().change_scene_to_file("res://dialogic/opening_scene.tscn")
 
 
 func _on_load_button_pressed():
